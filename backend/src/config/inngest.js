ec505528bd7f6e7431546c224644198c0955cf80
SoyebAhmed5 <soyebahmed.5632@gmail.com>
2026-07-14 23:13:13 +0530

    Stream and Sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack" });
@@ -29,7 +30,11 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    // TODO: DO MORE THINGS HERE
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
   },
 );
 
@@ -47,8 +52,6 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     await deleteStreamUser(id.toString());
-
-    // TODO: DO MORE THINGS HERE
   },
 );
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
+import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
@@ -11,10 +15,21 @@ app.use(express.json());
 app.use(clerkMiddleware()); //req.auth will be available in the request object
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+app.get("/debug-sentry1", (res, req) => {
+  throw new Error("First Sentry error!");
+});
+
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//   throw new Error("My first Sentry error!");
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
   try {
