d7466950b31afd2ca72fbcc6b9534a8614e44c50
SoyebAhmed5 <soyebahmed.5632@gmail.com>
2026-07-18 22:42:13 +0530

    Auth Page Implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import AuthPage from "./pages/AuthPage";
+import { Navigate, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <header>
-      <Show when="signed-out">
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal" />
-      </Show>
       <Show when="signed-in">
-        <UserButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </Routes>
+      </Show>
+
+      <Show when="signed-out">
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </Routes>
+        {/* <SignInButton mode="modal" /> */}
+        {/* <SignUpButton mode="modal" /> */}
       </Show>
     </header>
   );
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <UserButton /> Home page
+    </div>
+  );
+};
+
+export default HomePage;
